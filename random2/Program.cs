02d084c75adcc01e7f16a959dcbed4f64eabe317
raghavan.mk <raghavan.mk@gmail.com>
2019-08-20 21:05:13 +0530

    just committing

---
+++ random2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace random2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Digit d = new Digit(10);
+            byte number = d;
+            Console.WriteLine(number);
+            Digit d1 = (Digit) number;
+            Console.WriteLine(d1);
+            int? n = null;
+            Console.WriteLine(n);
+            NullableString(n.ToString());
+        }
+        #nullable enable
+        static string NullableString(string input){
+            return "";
+        }
+    }   
+
+    readonly struct Digit{
+        readonly byte digit;
+
+        public Digit(byte digit)  {
+            this.digit = digit;
+        }
+
+        public static implicit operator byte(Digit d) => d.digit;
+        public static explicit operator Digit(byte digit) => new Digit(digit);
+
+        public override string ToString() => $"{digit}";
+
+    }
+}
